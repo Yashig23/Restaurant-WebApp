4f971caa132ee269259e465315fe34c1daec6e6a
Yashi Gupta <guptayashi32003@gmail.com>
2024-05-29 20:08:32 +0530

    SignIn and signUP completed with full functionality

--- Restraunt/src/Pages/Singup/Signup.jsx
+++ Restraunt/src/Pages/Singup/Signup.jsx
@@ -1,6 +1,73 @@
 import React from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { getAuth, signInWithPopup } from 'firebase/auth'
+import { auth } from '../../constants/firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+import { GoogleAuthProvider } from 'firebase/auth'
+import { useNavigate} from 'react-router-dom'
+import { FacebookAuthProvider } from "firebase/auth";
 
 const Signup = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+
+    const handleData = async (e) => {
+        console.log("clicked");
+        e.preventDefault();
+        try {
+            const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredentials.user;
+            if (user) {
+                localStorage.setItem("userName", JSON.stringify(user));
+                console.log(user);
+                navigate('/');
+                return user;
+            } else {
+                console.log("Unsuccessful");
+            }
+        } catch (error) {
+            setError(error);  // Ensure setError is defined, or replace it with your error handling method
+            console.log(error, error.code, error.message);
+        }
+    };
+
+    const handleGoogleSignIn=async()=>{
+        console.log("clicked google");
+        // const auth = getAuth();
+        const provider = new GoogleAuthProvider();
+        try{
+            const result = await signInWithPopup(auth, provider);
+            // const credential = GoogleAuthProvider.credentialFromResult(result);
+            // const token = credential.accessToken;
+            const user = result.user
+            if(user){
+                console.log("Google sign-in Successful", user)
+                navigate('/');
+            }
+        }catch(error){
+            setError(error, "error during google sign-in");
+        }
+        
+    }
+
+    // const handleFacebookSignIn=async()=>{
+    //     const provider = new FacebookAuthProvider();
+    //     try{
+    //         const result = await signInWithPopup(auth, provider);
+    //         const user = result.user;
+    //         if(user){
+    //             console.log("Google sign-in Successful", user)
+    //             navigate('/');
+    //         }
+    //     }catch(error){
+    //         console.log("Error while signing in Facebook", error.code, error.message)
+    //     }
+    // }
+
   return <>
 
 <section class="bg-gray-100 min-h-screen flex box-border justify-center items-center" style={{backgroundImage: "url('https://t4.ftcdn.net/jpg/01/81/53/73/240_F_181537393_3LeVm5BDOdcmtIcqmIvlcwfV7jxLLqTF.jpg')", backgroundSize: 'cover'}}>
@@ -12,29 +79,24 @@ const Signup = () => {
             <h2 class="font-bold text-3xl text-[#412604]">SignUp</h2>
             <p class="text-sm mt-4 text-[#412604]">Not a member yet ? Easily SignUp now.</p>
 
-            <form action="" class="flex flex-col gap-4">
-                <input class="p-2 mt-8 rounded-xl border" type="text" name="Name" placeholder="Enter Name"/>
+            <form class="flex flex-col gap-4">
+                <input class="p-2 mt-8 rounded-xl border" type="text" name="name" placeholder="Enter Name" id="name" value={name} onChange={(e)=> setName(e.target.value)}/>
                 <div class="relative">
-                    <input class="p-2 rounded-xl border w-full" type="text" name="email" id="email" placeholder="Email"/>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-eye-slash-fill absolute top-1/2 right-3 -z-1 -translate-y-1/2 cursor-pointer hidden"
-                        id="mama" viewBox="0 0 16 16">
-                        <path
-                            d="m10.79 12.912-1.614-1.615a3.5 3.5 0 0 1-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 0 0 2.79-.588zM5.21 3.088A7.028 7.028 0 0 1 8 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 0 0-4.474-4.474L5.21 3.089z">
-                        </path>
-                        <path
-                            d="M5.525 7.646a2.5 2.5 0 0 0 2.829 2.829l-2.83-2.829zm4.95.708-2.829-2.83a2.5 2.5 0 0 1 2.829 2.829zm3.171 6-12-12 .708-.708 12 12-.708.708z">
-                        </path>
-                    </svg>
+                    <input class="p-2 rounded-xl border w-full" type="text" name="email" id="email" placeholder="Email" value={email} onChange={(e)=> setEmail(e.target.value)}/>
+                    <div class="relative">
+                    <input class="p-2 mt-5 rounded-xl border w-full" type="password" name="password" id="password" placeholder="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+                   
                 </div>
-                <button class="bg-[#412604] text-white py-2 rounded-xl hover:scale-105 duration-300 hover:bg-[#206ab1] font-medium" type="submit">SignUp</button>
+                 
+                </div>
+                <button class="bg-[#412604] text-white py-2 rounded-xl hover:scale-105 duration-300 hover:bg-[#206ab1] font-medium" onClick={handleData}>SignUp</button>
             </form>
             <div class="mt-6  items-center text-gray-100">
                 <hr class="border-gray-300" />
                 <p class="text-center text-sm">OR</p>
                 <hr class="border-gray-300"/>
             </div>
-            <button class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#60a8bc4f] font-medium">
+            <button onClick={handleGoogleSignIn} class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#60a8bc4f] font-medium">
                     <svg class="mr-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="25px">
                         <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
                         <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
@@ -44,12 +106,12 @@ const Signup = () => {
 
                     Login with Google
                 </button>
-                <button class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#60a8bc4f] font-medium">
+                {/* <button class="bg-white border py-2 w-full rounded-xl mt-5 flex justify-center items-center text-sm hover:scale-105 duration-300 hover:bg-[#60a8bc4f] font-medium">
                 <svg width="25px" class="mr-3" viewBox="0 0 48 48" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>Facebook-color</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Icons" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Color-" transform="translate(-200.000000, -160.000000)" fill="#153684">
                    <path d="M225.638355,208 L202.649232,208 C201.185673,208 200,206.813592 200,205.350603 L200,162.649211 C200,161.18585 201.185859,160 202.649232,160 L245.350955,160 C246.813955,160 248,161.18585 248,162.649211 L248,205.350603 C248,206.813778 246.813769,208 245.350955,208 L233.119305,208 L233.119305,189.411755 L239.358521,189.411755 L240.292755,182.167586 L233.119305,182.167586 L233.119305,177.542641 C233.119305,175.445287 233.701712,174.01601 236.70929,174.01601 L240.545311,174.014333 L240.545311,167.535091 C239.881886,167.446808 237.604784,167.24957 234.955552,167.24957 C229.424834,167.24957 225.638355,170.625526 225.638355,176.825209 L225.638355,182.167586 L219.383122,182.167586 L219.383122,189.411755 L225.638355,189.411755 L225.638355,208 L225.638355,208 Z" id="Facebook"> </path> </g> </g> </g></svg>
 
                     Login with FaceBook
-                </button>
+                </button> */}
 
             <div class="mt-4 text-sm flex justify-between items-center container-mr">
                 <p class="mr-3 md:mr-0 ">If you already have an account..</p>
