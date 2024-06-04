9d008e3e621754e8ba8ac6ccf3f12818b3fc688c
Yashi Gupta <guptayashi32003@gmail.com>
2024-06-05 00:02:17 +0530

    Navbar fixed

--- Restraunt/src/Components/Navbar/Navbar.jsx
+++ Restraunt/src/Components/Navbar/Navbar.jsx
@@ -11,37 +11,28 @@ const Navbar = () => {
 
   return<>
   <div>
-  <div className='flex w-full fixed  bg-transparent  py-1  justify-between'>
+  <div className='flex w-full fixed py-1 justify-between z-10' style={{backgroundImage: "url('https://i.pinimg.com/236x/6f/0d/de/6f0dde1a22fdc746b9abe8224735afae.jpg')", backgroundSize: 'cover'}}>
 
-    <div className=" flex w-full max-w-7xl bg-transparent pb-3 pt-3 px-5 rounded-full mx-auto ">
+    <div className=" flex w-full max-w-7xl pb-3 pt-3 px-5 rounded-full mx-auto ">
          <div className=' flex mr-auto '>
     <img src={logo} alt="Logo" className="max-w-20 max-h-20 absolute" />
   </div> 
         <ul className='sm:flex hidden list-none flex-column gap-10 '>
-  {navLinks.slice(0, 3).map((link) => (
+  {navLinks.map((link) => (
     <li
       key={link.id}
-      className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] text-yellow-500 font-medium cursor-pointer`}
+      className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] text-yellow-500 cursor-pointer`}
       onClick={() => setActive(link.title)}
     >
-      <a href={`#${link.id}`} className=" font-thin">{link.title}</a>
+      <a href={`${link.id}`} className=" font-medium">{link.title}</a>
     </li>
   ))}
-  
-  {navLinks.slice(3, 7).map((link) => (
-    <li
-      key={link.id}
-      className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] text-yellow-500 font-medium cursor-pointer`}
-      onClick={() => setActive(link.title)}
-    >
-      <a href={`#${link.id}`} className=" font-thin">{link.title}</a>
-    </li>
-  ))}
-</ul>
+
+ </ul> 
 
 
 <div className='flex ml-auto '>
-<button className='px-7  rounded-2xl text-[18px] font-medium py-1 text-white bg-yellow-500 hover:bg-yellow-600'> Login </button>
+<button onClick={()=>window.location.href='/login'}className='px-7  rounded-2xl text-[18px] font-medium py-1 text-brown-500 bg-yellow-500 hover:bg-yellow-600'> Login </button>
 </div>
 
     </div> 
@@ -57,7 +48,7 @@ const Navbar = () => {
             onClick={()=> {
             setToggle(!toggle);
             setActive(link.title)}}>
-              <a href={`#${link.id}`}>{link.title}</a>
+              <a href={`${link.id}`}>{link.title}</a>
             </li>
           ))}
         </ul>
